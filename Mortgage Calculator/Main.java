2412490efa4a6ee3af2b0e33bac8663e1313c636
programmer546 <bv.bv.bv@hotmail.com>
2022-03-10 01:12:04 -0500

    simplified the main class

--- Mortgage Calculator/Main.java
+++ Mortgage Calculator/Main.java
@@ -2,82 +2,21 @@
 //than upgrade the calculator for data validation on user input
 //advance the calculator such that it prints all the balances for the number of payments you have made
 //refactor it to an OOP design
-import java.text.NumberFormat;
+
 import java.util.Scanner;
 
 public class Main {
-    final static int MONTHS_IN_YEAR = 12;
-    final static int PERCENT = 100;
-
+    static Scanner typNow = new Scanner(System.in);
     public static void main(String[] args) {
-        int principal = (int) readNumber("Principal: ", 1000, 1_000_000);
-        double annualInterest = (double) readNumber("Annual Interest Rate: ", 1, 30);
-        int years = (int) readNumber("Period (Years): ", 1, 30);
 
-        printMortgage(principal, annualInterest, years);
-        printPaymentSchedule(principal, annualInterest, years);
-    }
 
-    private static void printMortgage(int principal, double annualInterest, int years) {
-        double mortgage = calculateMortgage(principal, annualInterest, years);
-        String mortgageView = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println();
-        System.out.println("MORTGAGE");
-        System.out.println("--------");
-        System.out.println("Monthly Payments: " + mortgageView);
-    }
-
-    private static void printPaymentSchedule(int principal, double annualInterest, int years) {
-        System.out.println();
-        System.out.println("PAYMENT SCHEDULE");
-        System.out.println("----------------");
-        for (short month = 1; month <= years * MONTHS_IN_YEAR; month++) {
-            double balance = calculateBalance(principal, annualInterest, years, month);
-            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
-        }
-    }
-
-    public static double readNumber(String prompt, double min, double max) {
-        Scanner scanner = new Scanner(System.in);
-        double value;
-        while (true) {
-            System.out.print(prompt);
-            value = scanner.nextDouble();
-            if (value >= min && value <= max)
-                break;
-            System.out.println("Enter a value between " + min + " and " + max);
-        }
-        return value;
-    }
-
-    public static double calculateBalance(
-            int principal,
-            double annualInterest,
-            int years,
-            short numberOfPaymentsMade
-    ) {
-        double monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-        double numberOfPayments = years * MONTHS_IN_YEAR;
-
-        double balance = principal
-                * (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
-                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
-
-        return balance;
-    }
-
-    public static double calculateMortgage(
-            int principal,
-            double annualInterest,
-            int years) {
-
-        double monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-        double numberOfPayments = years * MONTHS_IN_YEAR;
-
-        double mortgage = principal
-                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
-                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
-
-        return mortgage;
+        int principal = (int) UserInput.readNumber("Principal: ", 0,1000000);
+         double air = UserInput.readNumber("Annual Interest Rate: ", 0,30);
+        int period = (int) UserInput.readNumber("Period: ", 0,30);
+        double monthlyInterestRate = air / MortgageCalculator.PERCENT / MortgageCalculator.MONTHS_IN_A_YEAR;
+        int  numberOfPayments = period *12;
+        var calculator = new MortgageCalculator(principal, air, period);
+        new MortgageReport(calculator).printMortgage();
+        new MortgageReport(calculator).printPaymentSchedule();
     }
 }
